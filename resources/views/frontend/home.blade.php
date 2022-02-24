23a42d0c09c43b60ddd12c52cab8ce2907d0745b
Matheus Prado Rodrigues <matheusprador@gmail.com>
2022-02-24 01:04:08 -0300

    Initial commit

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/frontend.css">
+    <title>Laravel</title>
+</head>
+<body class="antialiased">
+<div id="header">
+    <header class="container">
+        <div class="row">
+            <div class="d-inline-block">
+                <a href="/">
+                    <h1 class="logo">
+                        <span>π</span>
+                        <span>MATH<br>MPR</span>
+                    </h1>
+                </a>
+            </div>
+            <div class="col">
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Home</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+</div>
+
+</body>
+</html>
