ee6da2d74f6aecb58aed99a1baa50cf303e23262
mathmpr <matheusprador@gmail.com>
2026-08-28 17:04:02 -0300

    fix problem with images

--- deploy/start-container.sh
+++ deploy/start-container.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 set -e
 
-mkdir -p storage bootstrap/cache
+mkdir -p \
+    storage/app/public \
+    storage/framework/cache/data \
+    storage/framework/sessions \
+    storage/framework/views \
+    storage/logs \
+    bootstrap/cache
+if [ ! -e public/storage ]; then
+    ln -s ../storage/app/public public/storage
+fi
 chown -R www-data:www-data storage bootstrap/cache
 
 php-fpm -D
 exec nginx -g "daemon off;"
-
